4195bb72a6cd5df9b990870dd4e1d7903a0fb878
bruno <bruno@tracki.gy>
2013-09-28 16:41:08 +0200

    backgrounds are moving!

--- main.lua
+++ main.lua
@@ -14,5 +14,6 @@ local physics = require("physics")
 physics.start()
 
 system.activate("multitouch")
+--load BGs images and set their positions
+local background = display.newImage("images/background.png")background.x = 240background.y = 160local midBackground = display.newImage("images/bgfar1.png")midBackground.x = 480midBackground.y = 160local function updateBackgrounds()	--move backgrounds	background.x = background.x - (.25)	midBackground.x = midBackground.x - 3endlocal function update(event)	updateBackgrounds()end-- call Update every 1 ms for an unlimited amount of time thanks to -1timer.performWithDelay(1, update, -1)
 
-print('Hello World')
