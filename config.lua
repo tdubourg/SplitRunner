2a62b520905ed5f68ca1c071d0053711539c3e56
bruno <bruno@tracki.gy>
2013-09-28 15:11:43 +0200

    First Config

---
+++ config.lua
@@ -0,0 +1,17 @@
+-- config.lua for project: SplitRun
+-- Managed with http://CoronaProjectManager.com
+-- Copyright 2013 . All Rights Reserved.
+-- cpmgen config.lua
+application =
+{
+	content =
+	{
+		width = 320,
+		height = 480,
+		scale = "letterbox",
+		fps = 30,
+		antialias = true,
+		xalign = "center",
+		yalign = "center"
+	}
+}
---
+++ main.lua
@@ -0,0 +1,18 @@
+-- Project: SplitRun
+-- Description:
+--
+-- Version: 1.0
+-- Managed with http://CoronaProjectManager.com
+--
+-- Copyright 2013 . All Rights Reserved.
+---- cpmgen main.lua
+
+
+local sprite = require("sprite")
+
+local physics = require("physics")
+physics.start()
+
+system.activate("multitouch")
+
+
